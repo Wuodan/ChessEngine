f7e7a010934389e2e4bfa2722e656723c96e0b4f
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2024-07-16 20:11:14 +0200

    Trying to speed up training

--- lib/training/training.py
+++ lib/training/training.py
@@ -1,20 +1,21 @@
 import logging
-
 import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-
 from lib.MovesAndPositions import MovesAndPositions
 from lib.model.Model import Model
+import time
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def get_device() -> torch.device:
-	return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	logging.info(f"Using device={device}")
+	return device
 
 
-def get_data_loader(moves_and_positions: MovesAndPositions, batch_size=32) -> DataLoader:
+def get_data_loader(moves_and_positions: MovesAndPositions, batch_size=4096, num_workers=4) -> DataLoader:
 	# transfer all data to GPU if available
 	device = get_device()
 	all_positions = np.array(moves_and_positions.positions)
@@ -25,21 +26,17 @@ def get_data_loader(moves_and_positions: MovesAndPositions, batch_size=32) -> Da
 	training_set = TensorDataset(positions, moves)
 
 	# Create data loaders for our datasets; shuffle for training, not for validation
-	return DataLoader(training_set, batch_size=batch_size, shuffle=True)
+	return DataLoader(training_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
 
 
 def train_one_epoch(
-		model: Model,
-		optimizer: torch.optim.SGD,
-		loss_fn: torch.nn.CrossEntropyLoss,
+		model: torch.nn.Module, optimizer: torch.optim.SGD, loss_fn: torch.nn.CrossEntropyLoss,
 		training_loader: DataLoader) -> float:
 	running_loss = 0.
 	last_loss = 0.
 
-	# Here, we use enumerate(training_loader) instead of
-	# iter(training_loader) so that we can track the batch
-	# index and do some intra-epoch reporting
 	for i, data in enumerate(training_loader):
+		start_time = time.time()
 
 		# Every data instance is an input + label pair
 		inputs, labels = data
@@ -59,74 +56,77 @@ def train_one_epoch(
 
 		# Gather data and report
 		running_loss += loss.item()
-		if i % 1000 == 999:
-			# loss per batch
-			last_loss = running_loss / 1000
-			# logging.info('  batch {} loss: {}'.format(i + 1, last_loss))
+
+		# Profiling each batch
+		batch_time = time.time() - start_time
+		if i % 100 == 99:
+			print(f"Batch {i + 1}: Time taken: {batch_time:.4f}s, Loss: {running_loss / 100:.4f}")
 			running_loss = 0.
 
 	return last_loss
 
 
 def save_best_model(vloss: float, path: str, output_folder: str) -> None:
-	f = open(output_folder + "/bestModel.txt", "w")
-	f.write(str(vloss))
-	f.write("\n")
-	f.write(path)
-	f.close()
+	with open(output_folder + "/bestModel.txt", "w") as f:
+		f.write(str(vloss))
+		f.write("\n")
+		f.write(path)
 	logging.info(f"NEW BEST MODEL FOUND WITH LOSS: {vloss}")
 
 
-def run_training(moves_and_positions: MovesAndPositions, output_folder: str,
-				 epochs=500, learning_rate=0.001, momentum=0.9):
-	best_loss = 10000000
-	epoch_number = 0
+def run_training(
+		moves_and_positions: MovesAndPositions, output_folder: str, epochs=500, learning_rate=0.001,
+		momentum=0.9, num_workers=4):
+	logging.info("Starting training")
 
-	training_loader = get_data_loader(moves_and_positions)
+	best_loss = 10000000
+
+	training_loader = get_data_loader(moves_and_positions, num_workers=num_workers)
+	logging.info("Got data-loader")
 
 	model = Model()
+	device = get_device()
+
+	if torch.cuda.device_count() > 1:
+		logging.info(f"Using {torch.cuda.device_count()} GPUs")
+		model = torch.nn.DataParallel(model)
+
+	model.to(device)
+
 	loss_fn = torch.nn.CrossEntropyLoss()
 	optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	model.to(device)
 
 	best_vloss = 1_000_000.
 
-	for _ in range(epochs):
+	for epoch_number in range(epochs):
+		epoch_start_time = time.time()
 
 		# Make sure gradient tracking is on, and do a pass over the data
 		model.train(True)
 		last_loss = train_one_epoch(model, optimizer, loss_fn, training_loader)
 
 		running_vloss = 0.0
-		# Set the model to evaluation mode, disabling dropout and using population
-		# statistics for batch normalization.
-
 		model.eval()
 
-		# Disable gradient computation and reduce memory consumption.
 		with torch.no_grad():
 			for i, vdata in enumerate(training_loader):
 				vinputs, vlabels = vdata
 				voutputs = model(vinputs)
-
 				vloss = loss_fn(voutputs, vlabels)
 				running_vloss += vloss
 
 		avg_vloss = running_vloss / (i + 1)
 
-		# Track the best performance, and save the model's state
 		if avg_vloss < best_vloss:
 			best_vloss = avg_vloss
 
-			# if better than previous best loss from all models created, save it
 			if best_loss > best_vloss:
 				model_path = output_folder + '/model'
 				torch.save(model.state_dict(), model_path)
 				save_best_model(best_vloss, model_path, output_folder)
 				best_loss = best_vloss
 
-		epoch_number += 1
-		logging.info(f"Epoch {epoch_number}")
+		epoch_time = time.time() - epoch_start_time
+		logging.info(f"Epoch {epoch_number}: Time taken: {epoch_time:.2f}s, Last loss: {last_loss:.4f}")
 
-	logging.info(f"\n\nBEST VALIDATION LOSS FOR ALL MODELS: {best_loss}", )
+	logging.info(f"\n\nBEST VALIDATION LOSS FOR ALL MODELS: {best_loss}")
